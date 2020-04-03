cbbd7a147b8eed3cdfdef0854ce04eca1184102d
adrian9380 <l.pava980709@itses.edu.mx>
2020-04-03 01:24:50 -0600

    Chart

--- Cafetera/src/app/folder/folder.module.ts
+++ Cafetera/src/app/folder/folder.module.ts
@@ -7,13 +7,14 @@ import { IonicModule } from '@ionic/angular';
 import { FolderPageRoutingModule } from './folder-routing.module';
 
 import { FolderPage } from './folder.page';
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    FolderPageRoutingModule
+    FolderPageRoutingModule, ChartsModule
   ],
   declarations: [FolderPage]
 })
--- Cafetera/src/app/folder/folder.page.ts
+++ Cafetera/src/app/folder/folder.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ChartDataSets } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
+import {HttpClient} from "@angular/common/http";
+
 
 @Component({
   selector: 'app-folder',
@@ -7,10 +11,58 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
+  // Data
+  chartData: ChartDataSets[] = [{ data: [], label: 'Stock price' }];
+  chartLabels: Label[];
+
+  // Options
+  chartOptions = {
+    responsive: true,
+    title: {
+      display: true,
+      text: 'Historic Stock price'
+    },
+    pan: {
+      enabled: true,
+      mode: 'xy'
+    },
+    zoom: {
+      enabled: true,
+      mode: 'xy'
+    }
+  };
+  chartColors: Color[] = [
+    {
+      borderColor: '#000000',
+      backgroundColor: '#ff00ff'
+    }
+  ];
+  chartType = 'line';
+  showLegend = false;
+
+  // For search
+  stock = '';
   public folder: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, public http: HttpClient ) { }
+  getData() {
+    this.http.get(`https://financialmodelingprep.com/api/v3/historical-price-full/${this.stock}?from=2018-03-12&to=2019-03-12`).subscribe(res => {
+      const history = res['historical'];
 
+      this.chartLabels = [];
+      this.chartData[0].data = [];
+
+      for (let entry of history) {
+        this.chartLabels.push(entry.date);
+        this.chartData[0].data.push(entry['close']);
+      }
+    });
+  }
+
+  typeChanged(e) {
+    const on = e.detail.checked;
+    this.chartType = on ? 'line' : 'bar';
+  }
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
   }
